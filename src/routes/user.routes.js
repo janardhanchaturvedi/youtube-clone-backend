83e82872ba067c05ffb8f5400591bf812d94af21
Janardhan <jan.cj84@gmail.com>
2024-11-02 23:43:03 +0530

    Feat : Added the login logout generate token and auth middleware functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser , logoutUser } from "../controllers/user.controller.js";
+
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 // router.post("/register", registerUser);
@@ -8,14 +10,19 @@ router.route("/register").post(
     upload.fields([
         {
             name: "avatar",
-            maxCount : 1
+            maxCount: 1,
         },
         {
-            name : "coverImage",
-            maxCount : 1
+            name: "coverImage",
+            maxCount: 1,
         },
     ]),
     registerUser
 );
+
+router.route("/login").post(loginUser);
+
+//secured router
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
